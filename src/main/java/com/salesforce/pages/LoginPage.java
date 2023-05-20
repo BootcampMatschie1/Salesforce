df8f69a78714db33b8f560f361bf7dea7a2465de
BootcampMatschie1 <bootcampmatschie@gmail.com>
2023-05-20 15:05:44 +0530

    Framework

---
+++ src/main/java/com/salesforce/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.salesforce.pages;
+
+import org.openqa.selenium.WebElement;
+
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
+
+public class LoginPage extends ProjectSpecificMethods {
+
+	public LoginPage enterUsername(String uName) {
+		// step1: locate the element
+		WebElement eleUsername = locateElement("username");
+
+		// step2: interact with the weblement
+		clearAndType(eleUsername, uName);
+		
+		return this;
+	}
+
+	public LoginPage enterPassword(String passWord) {
+
+		// step1: locate the element
+		WebElement elePassword = locateElement(Locators.XPATH,"//input[@id='password']");
+
+		// step2: interact with the weblement
+		clearAndType(elePassword, passWord);
+		
+		return this;
+
+	}
+
+	public HomePage clickLogin() {
+		
+		click(locateElement(Locators.CLASS_NAME, "decorativeSubmit"));
+		return new HomePage();
+
+	}
+
+}
